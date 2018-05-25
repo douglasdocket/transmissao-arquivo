f925ff966d3ab66d0ea0c27abbf8a3545598ebde
Douglas Miguel <douglas.miguel@cartorios.com.vc>
2018-05-25 13:03:06 -0300

    melhoria no codigo

--- src/main/java/br/com/docket/transmissaoarquivo/CarregarCSV.java
+++ src/main/java/br/com/docket/transmissaoarquivo/CarregarCSV.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class CarregarCSV {
+
+	public static void main(String[] args) {
+		new CarregarCSV().executarCarregamento();
+	}
 
 	public void executarCarregamento() {
 		Path path = Paths.get("layout-arquivo");
--- src/main/java/br/com/docket/transmissaoarquivo/ValidadorCSV.java
+++ src/main/java/br/com/docket/transmissaoarquivo/ValidadorCSV.java
@@ -1,6 +1,5 @@
 package br.com.docket.transmissaoarquivo;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,95 +8,101 @@ import java.util.stream.Collectors;
 
 public class ValidadorCSV {
 
+	public static void main(String[] args) {
+		new ValidadorCSV().executarValidacao();
+	}
+
 	public void executarValidacao() {
 		Path path = Paths.get("layout-arquivo");
 
-		try {
-			explorar(path);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+		explorar(path);
 	}
 
-	private void explorar(Path path) throws IOException {
+	private void explorar(Path path) {
 
-		if (Files.notExists(path)) {
-			return;
-		}
+		try {
 
-		if (Files.isHidden(path)) {
-			return;
-		}
+			if (Files.notExists(path)) {
+				return;
+			}
 
-		if (!Files.isReadable(path)) {
-			return;
-		}
+			if (Files.isHidden(path)) {
+				return;
+			}
 
-		if (Files.isDirectory(path)) {
-			List<Path> files = Files.list(path).collect(Collectors.toList());
+			if (!Files.isReadable(path)) {
+				return;
+			}
 
-			files.forEach(file -> {
-				try {
+			if (Files.isDirectory(path)) {
+				List<Path> files = Files.list(path).collect(Collectors.toList());
+
+				for (Path file : files) {
 					explorar(file);
-				} catch (IOException e) {
-					e.printStackTrace();
 				}
-			});
-		}
 
-		String fileName = path.getFileName().toString();
+			}
 
-		if (!fileName.endsWith(".csv")) {
+			String fileName = path.getFileName().toString();
+
+			if (!fileName.endsWith(".csv")) {
+				return;
+			}
+
+			System.out.println("\n=== Validação ===");
+			System.out.println("Arquivo: " + path);
+
+			List<String> lines = Files.lines(path).collect(Collectors.toList());
+
+			System.out.println("Qtd. de linhas: " + lines.size());
+
+			int quantidadeLinhasValidas = 0;
+			int quantidadeLinhasInvalidas = 0;
+
+			System.out.print("Linhas inválidas: ");
+
+			for (int i = 0; i < lines.size(); i++) {
+				String line = lines.get(i);
+
+				char[] chars = line.toCharArray();
+
+				int quantidadeVirgula = 0;
+
+				for (char aChar : chars) {
+
+					if (aChar == ',') {
+
+						quantidadeVirgula++;
+
+					}
+
+				}
+
+				if (quantidadeVirgula == 5) {
+					quantidadeLinhasValidas++;
+				}
+
+				if (quantidadeVirgula != 5) {
+					quantidadeLinhasInvalidas++;
+					String numeroLinha = String.valueOf(i + 1);
+					System.out.print(numeroLinha + ",");
+				}
+			}
+
+			System.out.println();
+
+			System.out.println("Qtd. de linhas válidas: " + quantidadeLinhasValidas);
+			System.out.println("Qtd. de linhas inválidas: " + quantidadeLinhasInvalidas);
+
+			System.out.println();
+
+			return;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
 			return;
 		}
-
-		System.out.println("\n=== Validação ===");
-		System.out.println("Arquivo: " + path);
-
-		List<String> lines = Files.lines(path).collect(Collectors.toList());
-
-		System.out.println("Qtd. de linhas: " + lines.size());
-
-		int quantidadeLinhasValidas = 0;
-		int quantidadeLinhasInvalidas = 0;
-
-		System.out.print("Linhas inválidas: ");
-
-		for (int i = 0; i < lines.size(); i++) {
-			String line = lines.get(i);
-
-			char[] chars = line.toCharArray();
-
-			int quantidadeVirgula = 0;
-
-			for (char aChar : chars) {
-
-				if (aChar == ',') {
-
-					quantidadeVirgula++;
-
-				}
-
-			}
-
-			if (quantidadeVirgula == 5) {
-				quantidadeLinhasValidas++;
-			}
-
-			if (quantidadeVirgula != 5) {
-				quantidadeLinhasInvalidas++;
-				String numeroLinha = String.valueOf(i + 1);
-				System.out.print(numeroLinha + ",");
-			}
-		}
-
-		System.out.println();
-
-		System.out.println("Qtd. de linhas válidas: " + quantidadeLinhasValidas);
-		System.out.println("Qtd. de linhas inválidas: " + quantidadeLinhasInvalidas);
-
-		System.out.println();
 
 	}
 
